2a03627ddf0fb974a312f2fef9478cc426b1efa3
Kim Jung yoon <skyepodium@gmail.com>
2022-03-17 17:01:51 +0900

    프로그래머스 - Ch14-2. BFS,DFS_가장 먼 노드 실습

--- main.js
+++ main.js
@@ -1,1 +1,49 @@
-console.log('a')
+function solution(n, edge) {
+    // 1. init
+    let res = 0
+    let MAX_DIST = 0
+    const check = new Array(n+1).fill(-1)
+    const v = Array.from(Array(n+1), () => [])
+
+    // 2. make graph
+    edge.forEach(ed => {
+        const [s, e] = ed
+        v[s].push(e)
+        v[e].push(s)
+    })
+
+    // 3. bfs
+    const bfs = (start) => {
+        const q = []
+        check[start] = 0
+        q.push(start)
+
+        while(q.length > 0) {
+            const node = q.shift()
+
+            v[node].forEach(nNode => {
+                if(check[nNode] === -1) {
+                    check[nNode] = check[node] + 1
+                    MAX_DIST = Math.max(MAX_DIST, check[nNode])
+                    q.push(nNode)
+                }
+            })
+        }
+    }
+
+    bfs(1)
+
+    // 4. count
+    check.forEach(dist => {
+        if(dist === MAX_DIST) res++
+    })
+
+    return res
+}
+
+const n = 6
+const vertex = [[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]
+
+const res = solution(n, vertex)
+
+console.log('res', res)
