f94c5f7e5027fbacf87866ac4206c95cb592594a
Kim Jung yoon <skyepodium@gmail.com>
2022-03-18 13:54:10 +0900

    프로그래머스 - 신고 결과 받기

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
-function solution(absolutes, signs) {
-    return absolutes.map((x, i) => signs[i] ? x : -x)
-            .reduce((p, c) => p+c)
+function solution(id_list, report, k) {
+    // 0. init
+    const res = []
+    const info = new Set()
+    const count = new Map()
+    const sent = new Map()
+    const mail = new Map()
+
+    // 1. set count
+    report.forEach(r => {
+        if(!info.has(r)) {
+            const [start, end] = r.split(" ")
+            count.has(end) ? count.set(end, count.get(end) + 1) : count.set(end, 1)
+            info.add(r)
+            sent.has(end) ? sent.set(end, sent.get(end).concat([start])) : sent.set(end, [start])
+        }
+    })
+
+    // 2. count check
+    for(const [key, value] of count.entries()) {
+        if(value >= k) {
+            sent.get(key).forEach(sender => mail.has(sender) ? mail.set(sender, mail.get(sender) + 1) : mail.set(sender, 1))
+        }
+    }
+
+    // 3. mail count
+    id_list.forEach(id => res.push(mail.has(id) ? mail.get(id) : 0))
+
+    return res
 }
-
-const res = solution([3, 4, 5], [true, false, false])
-
-console.log('res', res)
