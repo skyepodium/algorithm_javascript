18c0e4252bebfd2feb72f1f020d53e068052a53d
Kim Jung yoon <skyepodium@gmail.com>
2022-03-22 18:54:59 +0900

    leetcode - sort-array-by-increasing-frequency

--- main.js
+++ main.js
@@ -1,31 +1,25 @@
-const replaceDigits = (s) => {
+const frequencySort = (nums) => {
     // 1. init
-    const characters = s.split(/[0-9]+/).filter(x => x !== '')
-    const nums = s.split(/[a-z]+/).filter(x => x !== '')
-    const diff = Math.abs(characters.length - nums.length)
-    const minLength = Math.min(characters.length, nums.length)
-    const base = []
-    let res = ""
+    const m = new Map
+    const res = []
 
-    for(let i=0; i<minLength; i++) {
-        const c = characters[i]
-        const n = nums[i]
+    // 2. counter
+    nums.forEach(x => {
+        if(m.has(x)) m.set(x, m.get(x) + 1)
+        else m.set(x, 1)
+    })
 
-        const nOrder = (c.charCodeAt(0) - 'a'.charCodeAt(0) + Number(n)) % 26 + 'a'.charCodeAt(0)
-        base.push(String.fromCharCode(nOrder))
-    }
+    const sortedArr = [...m.entries()]
+                .sort((a, b) => a[1] === b[1] ? b[0] - a[0] : a[1] - b[1])
 
-    for(let i=0; i<base.length; i++) {
-        res += `${characters[i]}${base[i]}`
-    }
-
-    res += characters.slice(characters.length - diff, characters.length).join("")
+    sortedArr.forEach(x => {
+                    const [key, val] = x
+                    for(let i=0; i<val; i++) res.push(key)
+                })
 
     return res
 };
 
-// const s = "a1c1e1"
-const s = "a1b2c3d4e"
-const res = replaceDigits(s)
-
+const nums = [1,1,2,2,2,3]
+const res = frequencySort(nums)
 console.log('res', res)
