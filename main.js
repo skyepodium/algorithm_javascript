192dada22c07c26e823c9baf85fffbaf55d67763
Kim Jung yoon <skyepodium@gmail.com>
2022-03-22 14:39:34 +0900

    find-target-indices-after-sorting-array

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
-function solution(s) {
-    return (s.length === 4 || s.length === 6) && s.split(/[0-9]/).filter(x => x !== '').join("") === ""
-}
+const targetIndices = function(nums, target) {
+    // 1. init
+    nums.sort((a, b) => a - b)
+    const n = nums.length
+    const base = Array.from(Array(n).keys())
+    let res = []
+
+    // 2. lowerBound
+    const lowerBound = (target) => {
+        let l = 0
+        let r = n
+
+        while(l<r) {
+            const mid = l + Math.floor((r-l) / 2)
+            if(nums[mid] < target) l = mid + 1
+            else r = mid
+        }
+        return r
+    }
+
+    // 3. upperbound
+    const upperBound = (target) => {
+        let l = 0
+        let r = n
+
+        while(l<r) {
+            const mid = l + Math.floor((r-l) / 2)
+            if(nums[mid] <= target) l = mid + 1
+            else r = mid
+        }
+        return r
+    }
+
+    const l = lowerBound(target)
+    const r = upperBound(target) - 1
+    if(l <= r) res = base.slice(l, r+1)
+
+    return res
+};
