90ddb47fb360f285a31fc6e187e0bf0b52690786
skyepodium <skyepodium@gmail.com>
2022-03-27 07:55:20 +0900

    boats-to-save-people

--- main.js
+++ main.js
@@ -1,22 +1,33 @@
-const search = (nums, target) => {
+const numRescueBoats = (people, limit) => {
+    // 1. init
+    people.sort((a, b) => b-a)
+    let res = 0
+    let s = 0
+    let e = people.length - 1
 
-    const binarySearch = (s, e, nums, target) => {
-        if(s > e) return -1
-
-        const mid = s + ~~((e-s) / 2)
-        const cur = nums[mid]
-
-        if(cur < target) return binarySearch(mid+1, e, nums, target)
-        else if(cur === target) return mid
-        else return binarySearch(s, mid-1, nums, target)
+    // 2. loop
+    while(s <= e) {
+        const first = people[s]
+        if(s < e) {
+            const last = people[e]
+            if(first + last <= limit) e--
+        }
+        res++
+        s++
     }
 
-    return binarySearch(0, nums.length - 1, nums, target)
+    return res
 };
 
-const nums = [-1,0,3,5,9,12]
-const target = 9
+people = [1, 2]
+limit = 3
+people = [3,2,2,1]
+limit = 3
+people = [3,5,3,4]
+limit = 5
+people = [5, 1, 4, 2]
+limit = 6
 
-const res = search(nums, target)
+const res = numRescueBoats(people, limit)
 
 console.log('res', res)
