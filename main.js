72725050b1bda3e8ea4b02f19aa82bfde1a426eb
Kim Jung yoon <skyepodium@gmail.com>
2022-03-31 17:23:11 +0900

    참조 지역성

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-const numDifferentIntegers = (word) => {
-    return new Set(word.split(/[a-z]+/g)
-        .filter(x => x !== '')
-        .map(x => BigInt(x))).size
-};
+const countingSort = (arr) => {
+    const minVal = Math.min(...arr)
+    const maxVal = Math.max(...arr)
+    const res = []
+    const base = Array.from(Array(maxVal - minVal + 1)).fill(0)
+
+    arr.forEach(x => base[x - minVal]++)
+
+    base.forEach((b, idx) => {
+        while(b--) res.push(idx + minVal)
+    })
+
+    return res
+}
+
+arr = [5, 5, 2, 3, 4, 3, -1]
+
+arr = countingSort(arr)
+
+arr.forEach(x => console.log(x))
