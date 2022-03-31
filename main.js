56b8182f0ae3b217efa6cf61dd32896261008316
skyepodium <skyepodium@gmail.com>
2022-03-31 21:26:02 +0900

    leetcode - same tree

--- main.js
+++ main.js
@@ -1,13 +1,40 @@
-const missingNumber = (nums) => {
-    const s = new Set(nums)
+const isSameTree = (p, q) => {
+    let res = true
+    const dfs = (a, b) => {
+        console.log(a, b)
+        if((!a && b) || (b && !a)) {
+            res = false
+            return
+        }
+        else if(!a && !b) return
 
-    for(let i=0; i<=nums.length; i++) {
-        if(!s.has(i)) return i
+        if(a.left && b.left) dfs(a.left, b.left)
+        else if(a?.left?.val !== b?.left?.val) res = false
+        if(a.val !== b.val) res = false
+        if(a.right && b.right) dfs(a.right, b.right)
+        else if(a?.right?.val !== b?.right?.val) res = false
     }
+
+    dfs(p, q)
+
+    return res
 };
 
-nums = [3, 0, 1]
+class Node {
+    constructor(val) {
+        this.val = val
+        this.left = null
+        this.right = null
+    }
+}
 
-const res = missingNumber(nums)
+const first = new Node(1)
+first.left = new Node(2)
+
+const second = new Node(1)
+second.left = null
+second.right = new Node(2)
+
+const res = isSameTree(first, second)
 
 console.log('res', res)
