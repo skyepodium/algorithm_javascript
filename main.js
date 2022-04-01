ddb1e8273ca2313ff1a0957984b8bfbabe1f4a95
skyepodium <skyepodium@gmail.com>
2022-04-01 23:41:02 +0900

    leetcod - minstack with linked list

--- main.js
+++ main.js
@@ -1,21 +1,33 @@
-const isSameTree = (p, q) => {
-    let res = true
-    const dfs = (a, b) => {
-        console.log(a, b)
-        if((!a && b) || (b && !a)) {
-            res = false
-            return
-        }
-        else if(!a && !b) return
-
-        if(a.left && b.left) dfs(a.left, b.left)
-        else if(a?.left?.val !== b?.left?.val) res = false
-        if(a.val !== b.val) res = false
-        if(a.right && b.right) dfs(a.right, b.right)
-        else if(a?.right?.val !== b?.right?.val) res = false
+class Node {
+    constructor(val, minVal) {
+        this.val = val
+        this.next = null
+        this.minVal = minVal
     }
+}
 
-    dfs(p, q)
+const MinStack = function() {
+    this.head = null
+};
 
-    return res
+MinStack.prototype.push = function(val) {
+    const minVal = this.head ? Math.min(val, this.head.minVal) : val
+    const node = new Node(val, minVal)
+    node.next = this.head
+    this.head = node
+};
+
+
+MinStack.prototype.pop = function() {
+    this.head = this.head.next
+};
+
+
+MinStack.prototype.top = function() {
+    return this.head.val
+};
+
+
+MinStack.prototype.getMin = function() {
+    return this.head.minVal
 };
