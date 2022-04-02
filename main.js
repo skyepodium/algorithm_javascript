8fe47fa943c1a388923afed8e16b335e8225e95b
skyepodium <skyepodium@gmail.com>
2022-04-02 21:49:50 +0900

    leetcode - sign-of-the-product-of-an-array

--- main.js
+++ main.js
@@ -1,31 +1,19 @@
-const reverseVowels = (s) => {
-    // 1. init
-    const vowelList = []
-    const vowelSet = new Set("aeiouAEIOU".split(""))
-    const sList = s.split("")
+const arraySign = (nums) => {
+    const res = nums
+        .map(x => {
+            if(x === 0) return 0
+            else if(x > 0) return 1
+            else return -1
+        })
+        .reduce((prev, cur) => prev * cur)
 
-    // 2. make vowelList
-    for(let i=0; i<s.length; i++) {
-        const val = s[i]
-        if(vowelSet.has(val)) vowelList.push(i)
-    }
-
-    // 3. loop
-    let l = 0
-    let r = vowelList.length - 1
-    while(l < r) {
-        const lIdx = vowelList[l], rIdx = vowelList[r]
-        const temp = sList[lIdx]
-        sList[lIdx] = sList[rIdx]
-        sList[rIdx] = temp
-        l++
-        r--
-    }
-
-    return sList.join("")
+    if(res > 0) return 1
+    else if(res === 0) return 0
+    else return -1
 };
 
-const s = "hello"
-const res = reverseVowels(s)
+nums = [-1,-2,-3,-4,3,2,1]
+nums = [1,28,-91,-62,-36,-1,-84,-90,-92,61,6,-58,-60,2,51,-15,-18,-81,87,84,100,-84,-13,-87,-33,72,-72,-59,-79,28,-69,-97,-93,17,67,11,-12,19,5,42,-85,71,-77,-82,26,-58,-51,-14,63,-85,-86,66,47,57,-86,-25,-75,59,-17,-71,89,-78,-42,30,39,61,-96,-30,-29,-92,-90,69,20,65,32,92,44,-71,-18,87,-86,23,-96,-21,-49,-96,-98,35,-11,-1,81,-48,68,5,75,57,-30,-7,32,86,-29,-86,-61,45,-31,-93,-26,-9,86,57,-52,75,68,-58,14,27,-94,47,-38,-44,75,-83,21,-83,43,62,74,97,78,43,54,28,64,-19,-89,88,68,18,-96,-83,-25,-71,88,-84,-24,-61,72,-90,-56,29,46,56,51,16,66,-2,65,-95,16,51,42,61,99,89,-93,59,-99,69,26,-61,21,41,40,-4,-49,3,-96,57,65,72,-41,-77,-4,61,71,-88,21,-95,38,64,92,0,-63]
+const res = arraySign(nums)
 
 console.log('res', res)
