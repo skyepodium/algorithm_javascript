cd6e2fc616844ff6a37965ed70ab05a4336c33ee
skyepodium <skyepodium@gmail.com>
2022-04-04 19:36:11 +0900

    leetcode - decrypt-string-from-alphabet-to-integer-mapping

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
-const licenseKeyFormatting = (s, k) => {
-
-    s = s.replace(/-/g, "").reverse().toLowerCase()
-    const n = s.length
-
-    let t = ""
-    for(let i=0; i<n; i+=k) {
-        t += s.slice(i, i+k) + ""
+const freqAlphabets = (s) => {
+    const getAlphatbet = (n) => {
+        return String.fromCharCode("a".charCodeAt(0) + n - 1)
     }
 
-    t = t.slice(0, t.length - 1)
+    for(let i=26; i>=0; i--) {
+        const pattern = i >= 10 ? `${i}#` : `${i}`
+        const regex = new RegExp(pattern, 'g')
+        s = s.replace(regex, getAlphatbet(i))
+    }
 
-    return t.split("").reverse().join("")
+    return s
 };
+
+s = "10#11#12"
+s = "1326#"
+const res = freqAlphabets(s)
+
+console.log('res', res)
