a0cdadc3802a2db5f61ccda833511a771d738592
Kim Jung yoon <skyepodium@gmail.com>
2022-04-07 11:20:45 +0900

    leetcode - divide-array-into-equal-pairs

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-const minimumSum = (num) => {
-    const s = String(num).split("")
-        .map(x => Number(x))
-        .sort((a, b) => a - b)
+const divideArray = (nums) => {
+    const m = new Map()
 
+    nums.forEach(x => {
+        if(m.has(x)) m.set(x, m.get(x) + 1)
+        else m.set(x, 1)
+    })
 
-    return (s[0] + s[1]) * 10 + s[2] + s[3]
+    return [...m.values()].filter(x => x % 2 !== 0).length === 0
 };
+
+
+nums = [3,2,3,2,2,2]
+nums = [1, 2, 3, 4]
+const res = divideArray(nums)
+
+console.log('res', res)
