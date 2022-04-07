f473d44c71b6d28de7c5fd5cd5ccd8b27f678340
skyepodium <skyepodium@gmail.com>
2022-04-08 02:30:43 +0900

    프로그래머스 - 큰 수 만들기

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
-const prefixCount = (words, pref) => {
+const solution = (number, k) => {
+    // 1. init
+    const s = []
+    const numList = number.split("").map(x => Number(x))
 
-    return words.map(x => x.indexOf(pref) === 0 ? 1 : 0)
-        .reduce((prev, cur) => prev + cur)
-};
+    // 2. loop
+    for(const num of numList) {
+        while(s.length > 0 && s[s.length - 1] < num && k > 0) {
+            s.pop()
+            k--
+        }
+        s.push(num)
+    }
+    return s.slice(0, numList.length - k).join("")
+}
+
+
+number = "1924"
+k = 2
+
+number = "1231234"
+k = 3
+
+number = "4177252841"
+k = 4
+
+number = "21"
+k = 1
+const res = solution(number, k)
+
+console.log('res', res)
