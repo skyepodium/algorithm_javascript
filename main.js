96b8867e0b887babaa4c18acfe9b97ed34ba0d0a
skyepodium <skyepodium@gmail.com>
2022-04-08 01:57:47 +0900

    leetcode - counting-words-with-a-given-prefix

--- main.js
+++ main.js
@@ -1,21 +1,5 @@
-const sumOfUnique = (nums) => {
-    const m = new Map()
+const prefixCount = (words, pref) => {
 
-    nums.forEach(x => {
-        if(m.has(x)) m.set(x, m.get(x) + 1)
-        else m.set(x, 1)
-    })
-
-    return [...m.entries()].filter(x => x[1] === 1)
-                            .map(x => x[0])
-                            .reduce((prev, cur) => prev + cur, 0)
+    return words.map(x => x.indexOf(pref) === 0 ? 1 : 0)
+        .reduce((prev, cur) => prev + cur)
 };
-
-nums = [1,2,3,2]
-const res = sumOfUnique(nums)
-
-console.log('res', res)
-
-const a = []
-
-a.reduce((prev, cur) => prev + cur, 0)
