03ea49fef29e0171ce46a6b2a5134ec3c2331868
Kim Jung yoon <skyepodium@gmail.com>
2022-04-07 17:26:48 +0900

    leetcode - average-salary-excluding-the-minimum-and-maximum-salary

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-const lastStoneWeight = (stones) => {
-    // 1. loop
-    while(stones.length >= 2) {
-        stones.sort((a, b) => b-a)
-
-        const a = stones.shift()
-        const b = stones.shift()
-
-        stones.push(a===b ? 0 : Math.abs(a-b))
-    }
-
-    return stones[0]
+const average = (salary) => {
+    return (salary.reduce((prev, cur) => prev + cur) - Math.max(...salary) - Math.min(...salary)) / (salary.length - 2)
 };
-stones = [2,7,4,1,8,1]
-const res = lastStoneWeight(stones)
-
-console.log('res', res)
