b5118ebca9af56579a8b112e6ce2f4220bafc4dd
skyepodium <skyepodium@gmail.com>
2022-04-09 23:47:11 +0900

    정수 제곱근 판별

--- main.js
+++ main.js
@@ -1,21 +1,5 @@
 const solution = (n) => {
-    const d = Array.from(Array(n+1)).fill(1)
+    const num = ~~Math.sqrt(n)
 
-    const eratos = () => {
-        for(let i=2; i*i<=n; i++) {
-            for(let j=i*2; j<=n; j+=i) {
-                if(d[j] === 1) d[j] = 0
-            }
-        }
-    }
-
-    d[0] = 0
-    d[1] = 0
-    eratos()
-    return d.reduce((prev, cur) => prev + cur, 0)
+    return num * num === n ? (num + 1) * (num + 1) : -1
 }
-
-n = 10
-const res = solution(n)
-
-console.log('res', res)
