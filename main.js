247737db203d1e744c200f852ecced311a9904e3
skyepodium <skyepodium@gmail.com>
2022-04-25 00:17:33 +0900

    프로그래머스 - H-Index

--- main.js
+++ main.js
@@ -1,29 +1,28 @@
-const solution = n => {
+const solution = citations => {
     // 1. init
     let res = 0
-    const d = new Array(n+1)
-    d[0] = 0
-    for(let i=1; i<=n; i++) d[i] = d[i-1] + i
+    const maxVal = Math.max(...citations)
+    const n = citations.length
 
-    // 2. two pointer
-    let s = 0
-    let e = 0
-    while(s <= e && e < n + 1) {
-        const cur = d[e] - d[s]
-        if(cur <= n) {
-            if(cur === n) res++
-            e++
+    // 2. sort
+    citations.sort((a, b ) => a - b)
+
+    // 3. lowerBound
+    const lowerBound = (s, e, target) => {
+        while(s < e) {
+            const mid = s + ~~((e-s) / 2)
+            if(citations[mid] < target) s = mid + 1
+            else e = mid
         }
-        else{
-            s++
-        }
+        return e
+    }
+
+    // 4. loop
+    for(let i=0; i<=maxVal; i++) {
+        const idx = lowerBound(0, n, i)
+        const upper = n - idx
+        if(upper >= i) res = Math.max(res, i)
     }
 
     return res
 }
-
-n = 10000
-
-const res = solution(n)
-
-console.log('res', res)
