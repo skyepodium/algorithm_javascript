489a05eeb2c4d1a66c20829ff96f8244e604f2a6
skyepodium <skyepodium@gmail.com>
2022-04-24 16:50:37 +0900

    [1차] 프렌즈4블록

--- main.js
+++ main.js
@@ -1,66 +1,107 @@
-const solution = (begin, target, words) => {
+function solution(m, n, board) {
     // 1. init
-    const d = {}
-    const check = {}
-    words.push(begin)
+    let res = 0
+    const temp = m
+    m = n
+    n = temp
+    board = board.map(x => x.split(""))
+    let check = null
+    const d = [[0, 0], [0, 1], [1, 0], [1, 1]]
 
-    // 2. get diff
-    const getDiff = (a, b) => {
-        let res = 0
-        for(let i=0; i<a.length; i++) {
-            if(a[i] !== b[i]) res++
-        }
-        return res
+    // 2. init
+    const init = () => {
+        check = Array.from(Array(n), ()=> new Array(m).fill(false))
     }
 
-    // 3. loop
-    for(let i=0; i<words.length; i++) {
-        for(let j=0; j<words.length; j++) {
-            if(i === j) continue
-            const a = words[i]
-            const b = words[j]
+    // 3. search
+    const search = () => {
+        let cnt = 0
 
-            if(getDiff(a, b) === 1) {
-                if(d[a]) d[a] = [...d[a], b]
-                else d[a] = [b]
+        for(let i=0; i<n-1; i++) {
+            for(let j=0; j<m-1; j++) {
+                const prev = board[i][j]
+                if(prev === "") continue
 
-                if(d[b]) d[b] = [...d[b], a]
-                else d[b] = [a]
-            }
-        }
-    }
-
-    // 4. bfs
-    const bfs = start => {
-        const q = [start]
-        check[start] = 0
-
-        while(q.length > 0) {
-            const node = q.shift()
-            if(d[node]) {
-                for(const nNode of d[node]) {
-                    if(!check[nNode]) {
-                        check[nNode] = check[node] + 1
-                        q.push(nNode)
+                let isSame = true
+                for(const [dx, dy] of d) {
+                    const nx = i + dx
+                    const ny = j + dy
+                    const cur = board[nx][ny]
+                    if(prev !== cur) {
+                        isSame = false
+                        break
                     }
+                }
+
+                if(!isSame) continue
+
+                for(const [dx, dy] of d) {
+                    const nx = i + dx
+                    const ny = j + dy
+                    if(check[nx][ny]) continue
+                    check[nx][ny] = true
+                    cnt++
                 }
             }
         }
+
+
+
+        return cnt
     }
 
-    bfs(begin)
+    // 4. move
+    const move = () => {
+        for(let j=0; j<m; j++) {
+            for(let idx=n-1; idx>=0; idx--) {
+                let i = idx
+                if(board[i][j] === "" || check[i][j]) continue
 
-    return check[target] ? check[target] : 0
+                while(i < n-1 && board[i][j] !== "" && board[i+1][j] === "") {
+                    board[i+1][j] = board[i][j]
+                    board[i][j] = ""
+                    i++
+                }
+            }
+        }
+
+    }
+
+    // 5. clear
+    const clear = () => {
+        for(let i=0; i<n; i++)
+            for(let j=0; j<m; j++)
+                if(check[i][j]) board[i][j] = ""
+    }
+
+    // 6. loop
+    while(true) {
+        // 1) init
+        init()
+
+        // 2) search
+        const searchCnt = search()
+        if(searchCnt === 0) break
+        res += searchCnt
+
+        // 3) clear
+        clear()
+
+        // 4) move
+        move()
+    }
+
+    return res
 }
 
-begin = "hit"
-target = "cog"
-words = ["hot", "dot", "dog", "lot", "log", "cog"]
+m = 4
+n = 5
+board = ["CCBDE", "AAADE", "AAABF", "CCBBF"]
 
-begin = "hit"
-target = "cog"
-words = ["hot", "dot", "dog", "lot", "log"]
+// m = 6
+// n = 6
+// board = ["TTTANT", "RRFACC", "RRRFCC", "TRRRAA", "TTMMMF", "TMMTTJ"]
 
-const res = solution(begin, target, words)
+const res = solution(m, n, board)
 
 console.log('res', res)
