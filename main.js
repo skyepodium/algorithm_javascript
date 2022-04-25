bc63e4e76254e978fcc32d67d2d3d2cd25e22072
skyepodium <skyepodium@gmail.com>
2022-04-25 18:25:53 +0900

    프로그래머스 - 피로도

--- main.js
+++ main.js
@@ -1,28 +1,49 @@
-const solution = citations => {
+const solution = (k, dungeons) => {
     // 1. init
     let res = 0
-    const maxVal = Math.max(...citations)
-    const n = citations.length
+    const n = dungeons.length
+    const check = Array.from(Array(n).fill(false))
+    const permutation = []
 
-    // 2. sort
-    citations.sort((a, b ) => a - b)
-
-    // 3. lowerBound
-    const lowerBound = (s, e, target) => {
-        while(s < e) {
-            const mid = s + ~~((e-s) / 2)
-            if(citations[mid] < target) s = mid + 1
-            else e = mid
+    // 2. dfs
+    const dfs = (cnt, l) => {
+        if(cnt >= n) {
+            permutation.push([...l])
+            return
         }
-        return e
+
+        for(let i=0; i<n; i++) {
+            if(!check[i]) {
+                check[i] = true
+                l.push(i)
+                dfs(cnt + 1, l)
+                check[i] = false
+                l.pop()
+            }
+        }
     }
 
-    // 4. loop
-    for(let i=0; i<=maxVal; i++) {
-        const idx = lowerBound(0, n, i)
-        const upper = n - idx
-        if(upper >= i) res = Math.max(res, i)
+    dfs(0, [])
+
+    // 3. loop
+    for(const p of permutation) {
+        let l = k
+        let cnt = 0
+        for(const idx of p) {
+            const [a, b] = dungeons[idx]
+            if(l < a || l < b) break
+            l -= b
+            cnt++
+        }
+        res = Math.max(res, cnt)
     }
 
     return res
 }
+
+k = 80
+dungeons = [[80,20],[50,40],[30,10]]
+
+const res = solution(k, dungeons)
+
+console.log('res', res)
