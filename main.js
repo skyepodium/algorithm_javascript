d813682ec00f74fe92cdcd7f22579713fc69aed7
Kim Jung yoon <skyepodium@gmail.com>
2022-04-27 11:12:10 +0900

    프로그래머스 - 입국심사
    프로그래머스 - 입국심사

--- main.js
+++ main.js
@@ -1,42 +1,27 @@
-const minCostConnectPoints = (points) => {
+const solution = (n, times) => {
     // 1. init
-    const n = points.length
-    const e = []
-    let res = 0
-    const d = Array.from(Array(n).keys())
+    const maxVal = Math.max(...times)
+    let res = n * maxVal
+    let s = 0, e = maxVal * n
 
-    // 2. calDist
-    const calDist = (x, y, a, b) => {
-        return Math.abs(x - a) + Math.abs(y-b)
+    // 2. calTime
+    const calTime = a => {
+        return times.map(t => Math.trunc(a/t))
+            .reduce((prev, cur) => prev + cur)
     }
 
-    // 3. make graph
-    for(let i=0; i<n; i++) {
-        for(let j=i+1; j<n; j++) {
-            const [x, y] = points[i]
-            const [a, b] = points[j]
-            e.push([i, j, calDist(x, y, a, b)])
-        }
-    }
+    // 3. binarySearch
+    while(s < e) {
+        const mid = s + Math.trunc((e - s) / 2)
 
-    // 4. sort
-    e.sort((a, b) => a[2] - b[2])
-
-    // 5. find
-    const find = node => {
-        if(node === d[node]) return node
-        else return d[node] = find(d[node])
-    }
-
-    // 6. cost
-    for(let [a, b, cost] of e) {
-        a = find(a)
-        b = find(b)
-        if(a !== b) {
-            d[a] = b
-            res += cost
+        const cnt = calTime(mid)
+        if(cnt < n) {
+            s = mid + 1
+        }else {
+            res = Math.min(res, mid)
+            e = mid
         }
     }
 
     return res
-};
+}
