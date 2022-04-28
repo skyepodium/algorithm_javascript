054002ef0d85cb0136c42bfe93161543b9685470
skyepodium <skyepodium@gmail.com>
2022-04-28 19:52:13 +0900

    프로그래머스 - 경주로 건설

--- main.js
+++ main.js
@@ -1,27 +1,51 @@
-const solution = (n, times) => {
+const solution = board => {
     // 1. init
-    const maxVal = Math.max(...times)
-    let res = n * maxVal
-    let s = 0, e = maxVal * n
+    const n = board.length
+    const m = board[0].length
+    const maxVal = n * m * 600
+    const d = Array.from(new Array(n), () => Array.from(new Array(m), () => new Array(4).fill(maxVal)))
+    const dirList = [[0, -1], [0, 1], [1, 0], [-1, 0]]
 
-    // 2. calTime
-    const calTime = a => {
-        return times.map(t => Math.trunc(a/t))
-            .reduce((prev, cur) => prev + cur)
-    }
+    // 2. dijkstra
+    const dijkstra = (startX, startY) => {
+        const q = []
 
-    // 3. binarySearch
-    while(s < e) {
-        const mid = s + Math.trunc((e - s) / 2)
+        for(let i=0; i<4; i++) {
+            d[startX][startY][i] = 0
+            q.push([startX, startY, i, 0])
+        }
 
-        const cnt = calTime(mid)
-        if(cnt < n) {
-            s = mid + 1
-        }else {
-            res = Math.min(res, mid)
-            e = mid
+        while(q.length > 0) {
+            const [x, y, dir, cost] = q.shift()
+            if(cost > d[x][y][dir]) continue
+
+            for(let i=0; i<4; i++) {
+                const [dx, dy] = dirList[i]
+                const nx = x + dx
+                const ny = y + dy
+
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue
+                if(board[nx][ny] === 1) continue
+
+                const nCost = dir === i ? 100 : 600
+                if(d[nx][ny][i] > d[x][y][dir] + nCost) {
+                    d[nx][ny][i] = d[x][y][dir] + nCost
+                    q.push([nx, ny, i, d[nx][ny][i]])
+                }
+            }
         }
     }
 
-    return res
+    dijkstra(0, 0)
+
+    return Math.min(...d[n-1][m-1])
 }
+
+board = [[0,0,0],[0,0,0],[0,0,0]]
+
+const res = solution(board)
+
+console.log('res', res)
+
+n = 3
+m = 4
