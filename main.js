6413e4fb85a229dee4963c558ff764e246e602da
skyepodium <skyepodium@gmail.com>
2022-04-29 13:00:12 +0900

    leetcode - special-array-with-x-elements-greater-than-or-equal-x

--- main.js
+++ main.js
@@ -1,51 +1,40 @@
-const solution = board => {
+const specialArray = (nums) => {
     // 1. init
-    const n = board.length
-    const m = board[0].length
-    const maxVal = n * m * 600
-    const d = Array.from(new Array(n), () => Array.from(new Array(m), () => new Array(4).fill(maxVal)))
-    const dirList = [[0, -1], [0, 1], [1, 0], [-1, 0]]
+    const maxVal = Math.max(...nums)
+    let res = -1
 
-    // 2. dijkstra
-    const dijkstra = (startX, startY) => {
-        const q = []
-
-        for(let i=0; i<4; i++) {
-            d[startX][startY][i] = 0
-            q.push([startX, startY, i, 0])
+    // 2. calCnt
+    const calCnt = mid => {
+        let res = 0
+        for(const x of nums) {
+            if(x >= mid) res += 1
         }
+        return res
+    }
 
-        while(q.length > 0) {
-            const [x, y, dir, cost] = q.shift()
-            if(cost > d[x][y][dir]) continue
-
-            for(let i=0; i<4; i++) {
-                const [dx, dy] = dirList[i]
-                const nx = x + dx
-                const ny = y + dy
-
-                if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue
-                if(board[nx][ny] === 1) continue
-
-                const nCost = dir === i ? 100 : 600
-                if(d[nx][ny][i] > d[x][y][dir] + nCost) {
-                    d[nx][ny][i] = d[x][y][dir] + nCost
-                    q.push([nx, ny, i, d[nx][ny][i]])
-                }
-            }
+    // 3. binary search
+    let s = 0, e = maxVal
+    while(s <= e) {
+        const mid = s + Math.trunc((e - s) / 2)
+        const cnt = calCnt(mid)
+        if(cnt > mid) {
+            s = mid + 1
+        }
+        else if(cnt === mid) {
+            return mid
+        }
+        else {
+            e = mid - 1
         }
     }
 
-    dijkstra(0, 0)
+    return res
+};
 
-    return Math.min(...d[n-1][m-1])
-}
+nums = [3,5]
+// nums = [0,0]
+// nums = [0,4,3,0,4]
 
-board = [[0,0,0],[0,0,0],[0,0,0]]
-
-const res = solution(board)
+res = specialArray(nums)
 
 console.log('res', res)
-
-n = 3
-m = 4
