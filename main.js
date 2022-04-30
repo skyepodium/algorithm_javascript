97432f9d392b47793617a695b7ba95b8911cd059
skyepodium <skyepodium@gmail.com>
2022-04-30 19:14:44 +0900

    permutations-ii

--- main.js
+++ main.js
@@ -1,40 +1,39 @@
-const specialArray = (nums) => {
+const permuteUnique = (nums) => {
     // 1. init
-    const maxVal = Math.max(...nums)
-    let res = -1
+    const res = []
+    const s = new Set()
+    const n = nums.length
+    const check = new Array(n).fill(false)
 
-    // 2. calCnt
-    const calCnt = mid => {
-        let res = 0
-        for(const x of nums) {
-            if(x >= mid) res += 1
+    // 2. dfs
+    const dfs = (l, cnt) => {
+        if(cnt >= n) {
+            const key = l.join("")
+            if(!s.has(key)) {
+                s.add(key)
+                res.push([...l])
+            }
+            return
         }
-        return res
+
+        for(let i=0; i<n; i++) {
+            if(!check[i]){
+                l.push(nums[i])
+                check[i] = true
+                dfs(l, cnt + 1)
+                l.pop()
+                check[i] = false
+            }
+        }
     }
 
-    // 3. binary search
-    let s = 0, e = maxVal
-    while(s <= e) {
-        const mid = s + Math.trunc((e - s) / 2)
-        const cnt = calCnt(mid)
-        if(cnt > mid) {
-            s = mid + 1
-        }
-        else if(cnt === mid) {
-            return mid
-        }
-        else {
-            e = mid - 1
-        }
-    }
+    dfs([], 0)
 
     return res
 };
 
-nums = [3,5]
-// nums = [0,0]
-// nums = [0,4,3,0,4]
-
-res = specialArray(nums)
+nums = [1, 1, 2]
+nums = [1,2,3]
+res = permuteUnique(nums)
 
 console.log('res', res)
