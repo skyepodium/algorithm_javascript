37aa32481122ff21287ca0e373e336595872531f
skyepodium <skyepodium@gmail.com>
2022-05-01 15:25:12 +0900

    leetcode - backspace-string-compare- stack

--- main.js
+++ main.js
@@ -1,39 +1,19 @@
-const permuteUnique = (nums) => {
-    // 1. init
-    const res = []
-    const s = new Set()
-    const n = nums.length
-    const check = new Array(n).fill(false)
+const backspaceCompare = (s, t) => {
 
-    // 2. dfs
-    const dfs = (l, cnt) => {
-        if(cnt >= n) {
-            const key = l.join("")
-            if(!s.has(key)) {
-                s.add(key)
-                res.push([...l])
-            }
-            return
-        }
+    const refine = (s) => {
+        const st = []
 
-        for(let i=0; i<n; i++) {
-            if(!check[i]){
-                l.push(nums[i])
-                check[i] = true
-                dfs(l, cnt + 1)
-                l.pop()
-                check[i] = false
+        s.split("").forEach(x => {
+            if(x !== '#') {
+                st.push(x)
             }
-        }
+            else {
+                if(st.length > 0) st.pop()
+            }
+        })
+
+        return st.join("")
     }
 
-    dfs([], 0)
-
-    return res
+    return refine(s) === refine(t)
 };
-
-nums = [1, 1, 2]
-// nums = [1,2,3]
-res = permuteUnique(nums)
-
-console.log('res', res)
