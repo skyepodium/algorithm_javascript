828a371a1328fe637feadcd42230f350307bac6f
skyepodium <skyepodium@gmail.com>
2022-05-07 13:53:56 +0900

    leetcode - find-the-highest-altitude

--- index.ts
+++ index.ts
@@ -1,23 +1,13 @@
-const maxOperations = (nums: number[], k: number): number => {
+const largestAltitude = (gain) => {
     // 1. init
-    let res:number = 0
-    const m = new Map<number, number>();
-    nums.forEach(num => {
-        m.has(num) ? m.set(num, m.get(num) + 1) : m.set(num, 1)
-    })
+    let prev = 0
+    let res = 0
 
     // 2. loop
-    for(const num of nums) {
-        const remain:number = k - num
-
-        if(!m.has(remain)) continue
-        if(num === remain && m.get(remain) <= 1) continue
-        if(m.get(remain) < 1 || m.get(num) < 1) continue
-
-        res++
-        m.set(remain, m.get(remain) - 1)
-        m.set(num, m.get(num) - 1)
-    }
+    gain.forEach(g => {
+        prev = prev + g
+        res = Math.max(res, prev)
+    })
 
     return res
 };
