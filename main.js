9f1207e6bafb7bc5f13642c8fb3cf13c3296355a
skyepodium <skyepodium@gmail.com>
2022-05-14 17:37:51 +0900

    leetcode - network-delay-time - js

--- main.js
+++ main.js
@@ -1,19 +1,49 @@
-const smallerNumbersThanCurrent = (nums) => {
+const networkDelayTime = (times, n, k) => {
     // 1. init
-    const maxInt = 101
-    const c = Array.from(new Array(maxInt).fill(0))
-    const d = Array.from(new Array(maxInt).fill(0))
+    const maxVal = 10001
+    let res = 0
+    const d = Array.from(new Array(n + 1).fill(maxVal))
+    const v = Array.from(Array(n + 1), () => [])
 
-    // 2. loop
-    nums.forEach(num => c[num]++)
+    // 2. make graph
+    times.forEach(([s, e, c]) => v[s].push([c, e]))
 
-    let prev = 0
-    for(let i=0; i<maxInt; i++) {
-        if(c[i] === 0) continue
+    // 3. dijkstra
+    const dijkstra = (startNode) => {
+        const q = []
+        d[startNode] = 0
+        q.push([startNode, d[startNode]])
 
-        d[i] = prev
-        prev += c[i]
+        while(q.length > 0) {
+            const [node, cost] = q.shift()
+
+            if(d[node] < cost) continue
+
+            for(const [nCost, nNode] of v[node]) {
+                if(d[nNode] > d[node] + nCost) {
+                    d[nNode] = d[node] + nCost
+                    q.push([nNode, d[nNode]])
+                }
+            }
+        }
     }
 
-    return nums.map(num => d[num])
+    dijkstra(k)
+
+    // 4. check
+    res = Math.max(...d.slice(1, d.length))
+
+    return res === maxVal ? - 1 : res
 };
+
+times = [[2,1,1],[2,3,1],[3,4,1]]
+n = 4
+k = 2
+
+// times = [[1,2,1],[2,1,3]]
+// n = 2
+// k = 2
+
+const res = networkDelayTime(times, n, k)
+
+console.log('res', res)
