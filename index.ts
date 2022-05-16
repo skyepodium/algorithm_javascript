6b2e8c70ec0857ded833620c24c8ff904869d4b8
skyepodium <skyepodium@gmail.com>
2022-05-16 20:06:00 +0900

    leetcode - shortest-path-in-binary-matrix

--- index.ts
+++ index.ts
@@ -1,24 +1,34 @@
-const deepestLeavesSum = (root: TreeNode | null): number => {
+const shortestPathBinaryMatrix = (grid: number[][]): number => {
     // 1. init
-    let d: number = 0
-    let res: number = 0
+    const n: number = grid.length
+    const m: number = grid[0].length
+    const check: number[][] = Array.from(new Array(n), () => new Array(m).fill(-1))
+    const d: number[][] = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]]
 
-    // 2. dfs
-    const dfs = (node: TreeNode, depth: number) => {
-        if(!node) return
-
-        if(depth === d) res += node.val
-
-        if(depth > d) {
-            res = node.val
-            d = depth
+    // 2. bfs
+    const bfs = (startX:number, startY:number): number => {
+        const q: [number, number][] = []
+        if(grid[startX][startY] === 0) {
+            check[startX][startY] = 1
+            q.push([startX, startY])
         }
 
-        dfs(node.left, depth + 1)
-        dfs(node.right, depth + 1)
+        while(q.length > 0) {
+            const [x, y] = q.shift()
+
+            for(const [dx, dy] of d) {
+                const nx = x + dx
+                const ny = y + dy
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue
+                if(grid[nx][ny] !== 0 || check[nx][ny] !== -1) continue
+
+                check[nx][ny] = check[x][y] + 1
+                q.push([nx, ny])
+            }
+        }
+
+        return check[n-1][m-1]
     }
 
-    dfs(root, 0)
-
-    return res
+    return bfs(0, 0)
 };
--- main.js
+++ main.js
@@ -1,24 +1,34 @@
-const deepestLeavesSum = (root) => {
+const shortestPathBinaryMatrix = (grid) => {
     // 1. init
-    let d = 0
-    let res = 0
+    const n = grid.length
+    const m = grid[0].length
+    const check = Array.from(new Array(n), () => new Array(m).fill(-1))
+    const d = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]]
 
-    // 2. dfs
-    const dfs = (node, depth) => {
-        if(!node) return
-
-        if(depth === d) res += node.val
-
-        if(depth > d) {
-            res = node.val
-            d = depth
+    // 2. bfs
+    const bfs = (startX, startY) => {
+        const q = []
+        if(grid[startX][startY] === 0) {
+            check[startX][startY] = 1
+            q.push([startX, startY])
         }
 
-        dfs(node.left, depth + 1)
-        dfs(node.right, depth + 1)
+        while(q.length > 0) {
+            const [x, y] = q.shift()
+
+            for(const [dx, dy] of d) {
+                const nx = x + dx
+                const ny = y + dy
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue
+                if(grid[nx][ny] !== 0 || check[nx][ny] !== -1) continue
+
+                check[nx][ny] = check[x][y] + 1
+                q.push([nx, ny])
+            }
+        }
+
+        return check[n-1][m-1]
     }
 
-    dfs(root, 0)
-
-    return res
+    return bfs(0, 0)
 };
