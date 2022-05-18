55dbc126f603b8ebaa0bd1034ae576854d4e09d3
skyepodium <skyepodium@gmail.com>
2022-05-18 20:47:45 +0900

    leetcode - build-array-from-permutation

--- index.ts
+++ index.ts
@@ -1,34 +1,3 @@
-const shortestPathBinaryMatrix = (grid: number[][]): number => {
-    // 1. init
-    const n: number = grid.length
-    const m: number = grid[0].length
-    const check: number[][] = Array.from(new Array(n), () => new Array(m).fill(-1))
-    const d: number[][] = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]]
-
-    // 2. bfs
-    const bfs = (startX:number, startY:number): number => {
-        const q: [number, number][] = []
-        if(grid[startX][startY] === 0) {
-            check[startX][startY] = 1
-            q.push([startX, startY])
-        }
-
-        while(q.length > 0) {
-            const [x, y] = q.shift()
-
-            for(const [dx, dy] of d) {
-                const nx = x + dx
-                const ny = y + dy
-                if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue
-                if(grid[nx][ny] !== 0 || check[nx][ny] !== -1) continue
-
-                check[nx][ny] = check[x][y] + 1
-                q.push([nx, ny])
-            }
-        }
-
-        return check[n-1][m-1]
-    }
-
-    return bfs(0, 0)
+const buildArray = (nums: number[]): number[] => {
+    return nums.map((val, idx) => nums[nums[idx]])
 };
--- main.js
+++ main.js
@@ -1,34 +1,10 @@
-const shortestPathBinaryMatrix = (grid) => {
-    // 1. init
-    const n = grid.length
-    const m = grid[0].length
-    const check = Array.from(new Array(n), () => new Array(m).fill(-1))
-    const d = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]]
-
-    // 2. bfs
-    const bfs = (startX, startY) => {
-        const q = []
-        if(grid[startX][startY] === 0) {
-            check[startX][startY] = 1
-            q.push([startX, startY])
-        }
-
-        while(q.length > 0) {
-            const [x, y] = q.shift()
-
-            for(const [dx, dy] of d) {
-                const nx = x + dx
-                const ny = y + dy
-                if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue
-                if(grid[nx][ny] !== 0 || check[nx][ny] !== -1) continue
-
-                check[nx][ny] = check[x][y] + 1
-                q.push([nx, ny])
-            }
-        }
-
-        return check[n-1][m-1]
-    }
-
-    return bfs(0, 0)
+const buildArray = (nums) => {
+    return nums.map((val, idx) => nums[nums[idx]])
 };
+
+nums = [0,2,1,5,3,4]
+nums = [5,0,1,2,3,4]
+
+const res = buildArray(nums)
+
+console.log('res', res)
