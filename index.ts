db989d19556c0d329544cd81c77e5250775008d6
skyepodium <skyepodium@gmail.com>
2022-05-21 12:06:49 +0900

    leetcode - coin-change

--- index.ts
+++ index.ts
@@ -1,3 +1,16 @@
-const containsDuplicate = (nums: number[]): boolean => {
-    return new Set(nums).size !== nums.length
+const coinChange = (coins: number[], amount: number): number => {
+    // 1. init
+    const d:number[] = Array.from(new Array(amount+1).fill(amount+1))
+    d[0] = 0
+
+    // 2. bottom up
+    for(let i=1; i<=amount; i++) {
+        for(const c of coins) {
+            if(i-c < 0) continue
+
+            d[i] = Math.min(d[i], d[i-c] + 1)
+        }
+    }
+
+    return d[amount] <= amount ? d[amount] : -1
 };
--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-const containsDuplicate = (nums) => {
-    return new Set(nums).size !== nums.length
+const coinChange = (coins, amount) => {
+    // 1. init
+    const d = Array.from(new Array(amount+1).fill(amount+1))
+    d[0] = 0
+
+    // 2. bottom up
+    for(let i=1; i<=amount; i++) {
+        for(const c of coins) {
+            if(i-c < 0) continue
+
+            d[i] = Math.min(d[i], d[i-c] + 1)
+        }
+    }
+
+    return d[amount] <= amount ? d[amount] : -1
 };
-
-nums = [1,2,3,1]
-
-const res = containsDuplicate(nums)
-
-console.log('res', res)
